99c2670a153e1c70bb91e086fce7c18f1971c38e
Bitingo Josaphat JB <85907186+Josaphat12-tech@users.noreply.github.com>
2022-05-14 11:20:59 +0200

    Add files via upload
    Basic beginners contract creation and deployment

---
+++ contracts...SimpleStorage.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage{
+    // uint256 favoriteNumber = 5;
+    // bool favoriteBool = false;
+    // string favoriteString = 'String';
+    // int256 favoriteInt = -5;
+    // address favoriteAddress = 0x57aA44F2AC2f4909B50F7fF220516Faa009B88B8;
+    // bytes32 favoriteBytes = 'cat';
+
+
+    uint256 favoriteNumber;  //this variable will get initialized to 
+    bool favoriteBool;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // People public person =People({favoriteNumber: 2, name:"Josaphat"});
+
+
+    // This is how we create a dynamic array in solidity
+    People[] public people;
+    mapping(string => uint256) public nameToFavortieNumber;
+    // mapping(uint256 => string) public favoriteNumberToName;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // This function retrieve data inside the variables
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+
+    // Function to addPerson inside the Array 
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        // people.push(People({favoriteNumber:_favoriteNumber, name:_name}));
+        people.push(People(_favoriteNumber,_name));
+        nameToFavortieNumber[_name] = _favoriteNumber;
+        // favoriteNumberToName[_favoriteNumber] = _name;
+    }
+}
